a136a6a53b24be04f5b86b7b21e646b375998b74
Ben <algorithmnblues92@gmail.com>
2023-03-23 20:45:08 -0700

    added nutrition facts fetch

--- assets/app.js
+++ assets/app.js
@@ -105,13 +105,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-function testBtnF() {
-    console.log("test btn");
-}
 
       var resultCounterV = 0;
+      var retryV = 0;
 
-    function searchBtnF() {
+    async function searchBtnF() {
     console.log("search button clicked");
 
     // //adding button with all necessary attributes
@@ -135,7 +133,7 @@ function testBtnF() {
     }
 
 
-    fetch("https://api.fda.gov/food/enforcement.json?limit=1000").then((res) => res.json()).then(function (data) {
+    await fetch("https://api.fda.gov/food/enforcement.json?limit=1000").then((res) => res.json()).then(function (data) {
 
       for(x=0;x<data.results.length; x++) {
         if (data.results[x].status == "Terminated") {
@@ -217,6 +215,144 @@ function testBtnF() {
     termDate.textContent = searchResultArray[i].termination_date;
     console.log("result counter variable :" + resultCounterV);
       }
-    })
+    }
+
+    ).catch((error) => {SearchBtnF()})
   
   }
+
+
+var resultCounterV2 = 0;
+
+async function nutritionFactSearchBtnF() {
+
+var searchInputValue2 = document.getElementById("form1").value;
+
+document.getElementById("cardContainerNutritionError").style.display = "none";
+
+        if (resultCounterV2 > 0) {
+      console.log("result count if statement procced")
+      for (let s=0; s<resultCounterV2; s++) {
+        console.log("resultCounterV2 :" + resultCounterV2);
+        document.getElementById("cardContainerNutrition").removeChild(document.getElementById("cardEl" + s));
+        console.log("remove child :" + ("cardEl" + s));
+      }
+      resultCounterV2 = 0;
+    }
+
+
+  await fetch("https://api.api-ninjas.com/v1/nutrition?query=" + searchInputValue2, 
+  {headers:{"X-Api-Key":"EylXzrZXiY+QZVYcYKkVGg==N5f12J1v0qB2Gly9"}}) 
+  .then((res) => res.json())
+  .then(function (data) {
+
+    console.log(data);
+    console.log("name :" + data[0].name);
+    console.log("serving size :" + data[0].serving_size_g + " g");
+    console.log("calories :" + data[0].calories);
+    console.log("fat total :" + data[0].fat_total_g + " g");
+    console.log("cholesterol :" + data[0].cholesterol_mg + " mg");
+    console.log("sodium :" + data[0].sodium_mg + " mg");
+    console.log("carbohydrates :" + data[0].carbohydrates_total_g + " g");
+    console.log("fiber :" + data[0].fiber_g + " g");
+    console.log("sugar :" + data[0].sugar_g + " g");
+    console.log("protein :" + data[0].protein_g + " g");
+    console.log("potassium :" + data[0].potassium_mg + " mg");
+ 
+
+    for(i=0;i<data.length; i++) {
+
+    var cardCon = document.getElementById("cardContainerNutrition");
+    var card = document.createElement("div");
+    var nameFood = document.createElement("div");
+    var servingSizeG = document.createElement("div");
+    var calories = document.createElement("div");
+    var fatTotal = document.createElement("div");
+    var cholesterol = document.createElement("div");
+    var sodium = document.createElement("div");
+    var carbohydrates = document.createElement("div");
+    var fiber = document.createElement("div");
+    var sugar = document.createElement("div");
+    var protein = document.createElement("div");
+    var potassium = document.createElement("div");
+    // var searchBtnEl = document.createElement("button");
+
+    card.setAttribute("id", "cardEl" + i);
+    nameFood.setAttribute("id", "nameFoodEl" + i);
+    servingSizeG.setAttribute("id", "servingSizeGEl" + i);
+    calories.setAttribute("id", "caloriesEl" + i);
+    fatTotal.setAttribute("id", "fatTotalEl" + i);
+    cholesterol.setAttribute("id", "cholesterolEl" + i);
+    sodium.setAttribute("id", "sodium" + i);
+    carbohydrates.setAttribute("id", "carbohydratesEl" + i);
+    fiber.setAttribute("id", "fiberEl" + i);
+    sugar.setAttribute("id", "fiberEl" + i);
+    protein.setAttribute("id", "fiberEl" + i);
+    potassium.setAttribute("id", "fiberEl" + i);
+    // searchBtnEl.setAttribute("id", "searchButton");
+
+    cardCon.appendChild(card);
+    card.appendChild(nameFood);
+    card.appendChild(servingSizeG);
+    card.appendChild(calories);
+    card.appendChild(fatTotal);
+    card.appendChild(cholesterol);
+    card.appendChild(sodium);
+    card.appendChild(carbohydrates);
+    card.appendChild(fiber);
+    card.appendChild(sugar);
+    card.appendChild(protein);
+    card.appendChild(potassium);
+    // document.getElementById("searchContainer").appendChild(searchBtnEl);
+
+    card.style.display = "flex";
+    card.style.flexDirection = "column";
+    card.style.alignItems = "column";
+    card.style.flexWrap = "wrap";
+    card.style.width = "500px";
+    card.style.height = "400px";
+    card.style.margin = "70px";
+    card.style.border = "2px solid dodgerblue";
+    card.style.backgroundColor = "springgreen";
+
+    nameFood.style.backgroundColor = "pink";
+
+    servingSizeG.style.backgroundColor = "magenta";
+
+
+    nameFood.textContent = "name :" + data[i].name;
+    servingSizeG.textContent = "serving size :" + data[i].serving_size_g + " g";
+    calories.textContent = "calories :" + data[i].calories;
+    fatTotal.textContent = "fat total :" + data[i].fat_total_g + " g";
+    cholesterol.textContent = "cholesterol :" + data[i].cholesterol_mg + " mg";
+    sodium.textContent = "sodium :" + data[i].sodium_mg + " mg";
+    carbohydrates.textContent = "carbohydrates :" + data[i].carbohydrates_total_g + " g";
+    fiber.textContent = "fiber :" + data[i].fiber_g + " g";
+    sugar.textContent = "sugar :" + data[i].sugar_g + " g";
+    protein.textContent = "protein :" + data[i].protein_g + " g";
+    potassium.textContent = "potassium :" + data[i].potassium_mg + " mg";
+
+
+    resultCounterV2++;
+
+    console.log("result Counter V2 :" + resultCounterV2);
+
+}
+
+}
+
+).catch((error) => {
+    
+    if (retryV < 10) {
+    retryV++;
+    nutritionFactSearchBtnF();
+} else {
+    document.getElementById("cardContainerNutritionError").style.display = "flex";
+    retryV = 0;
+}
+
+}
+);
+
+}
+
